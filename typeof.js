4fe337eeffb4ca3aae7f35f4742067911c1f760f
manjuladevim23-rgb <manjuladevi.m23@gmail.com>
2026-02-05 23:05:57 +0530

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,11 @@
+let userName ="First"
+let Organization ="SMC2"
+let contactNumber = 1234567890
+let knowsAutomation =true
+let userplaywright
+
+console.log(`the username is  ${userName} and type is ${typeof(userName)}`)
+console.log(`the Organization is  ${Organization} and type is ${typeof(Organization)}`)
+console.log(`the contactNumber is  ${contactNumber} and type is ${typeof(contactNumber)}`)
+console.log(`Knows Automation is  ${knowsAutomation} and type is ${typeof(knowsAutomation)}`)
+console.log(`the playright user is  ${userplaywright} and type is ${typeof(userplaywright)}`)
